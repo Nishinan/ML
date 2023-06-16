651f10a5c4b4685ab0cfbdb46e40285155a936ac
April <nsn-0021@sjtu.edu.cn>
2023-06-17 00:51:53 +0800

    R

--- task2/compare.py
+++ task2/compare.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pickle
 import random
-import torch
-import torch.nn as nn
+import torch 
+import torch.nn as nn 
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
 from torch_geometric.data import Data, Dataset, DataLoader
